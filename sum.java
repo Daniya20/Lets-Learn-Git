9c74b74bebcd41639cecd7763e12b5d15740a5b2
Daniya khan <khan.daniya16994@gmail.com>
2024-01-18 10:04:52 +0530

    committing new file

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public clas sum{
         int a=5;
         int b=6;
         System.out.println("Sum of "+a+" and "+b+ " is ="a+b);
+        System.out.println("Daniya here i am modifying it");
     }
 
 }
