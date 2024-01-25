bf24b48af645774ad61af957cf6339a26843d77a
Daniya khan <khan.daniya16994@gmail.com>
2024-01-25 09:23:57 +0530

    multiply file committed

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class difference{
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        System.out.println("Multiplication of "+a+" and "+b+ " is ="a*b);
+        
+    }
+
+}
